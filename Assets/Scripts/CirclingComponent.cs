5641b6489fea60bd6523e8e2a4cd317f18ca7b63
Darkchicken <joshshucker@hotmail.com>
2017-02-11 15:58:35 -0800

    Initial work
    Trail draws successfully
    Appears on click, resets on release
    Enemies receive event when player clicks and releases

---
+++ Assets/Scripts/CirclingComponent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CirclingComponent : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        PlayerController.OnClick += CalculateCircling;
+        PlayerController.OnRelease += CheckIfCircled;
+    }
+
+    private void OnDisable()
+    {
+        PlayerController.OnClick -= CalculateCircling;
+        PlayerController.OnRelease -= CheckIfCircled;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CalculateCircling()
+    {
+        Debug.Log("Calculating if " + gameObject.name + " is being circled");
+    }
+
+    void CheckIfCircled()
+    {
+        Debug.Log("Mouse released, check if " + gameObject.name + " should be destroyed");
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public delegate void ClickAction();
+    public static event ClickAction OnClick;
+    public static event ClickAction OnRelease;
+
+    TrailRenderer trail;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        trail = GetComponent<TrailRenderer>();
+        trail.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        FollowMouse();
+        CheckMouseClick();
+	}
+
+    private void FollowMouse()
+    {
+        Vector3 screenPoint = Input.mousePosition;
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPoint.z = transform.position.z;
+        transform.position = worldPoint;
+        //transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
+    }
+
+    private void CheckMouseClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            trail.enabled = true;
+            OnClick();
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            trail.Clear();
+            trail.enabled = false;
+            OnRelease();
+        }
+    }
+
+   
+}
