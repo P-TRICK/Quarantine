cf9aa1cde336ee9f54c4d0e9753ed810aa3433ab
Darkchicken <joshshucker@hotmail.com>
2017-02-12 13:11:29 -0800

    Finalized circle function
    Each object with CirclingComponent checks if circled while click is held
    If the angle around object by mouse exceeds the object's angle limit,
    object is destroyed

--- Assets/Scripts/CirclingComponent.cs
+++ Assets/Scripts/CirclingComponent.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Author: Josh Shucker
+ * Circling component can be attached to any object
+ * Receives event call from Player to start caclulations
+ * Calculates total angle of mouse movement around object
+ * Completes calculation when it receives event call from Player to stop
+ * If total angle is greater than its angle limit, it assumes it was circled and destroys object
+ */
+
 public class CirclingComponent : MonoBehaviour
 {
+    // the total angle of the mouse movement around the object
+    private float _totalAngle = 0;
+    // the limit the total angle must surpass to count as circling the object
+    private const float ANGLE_LIMIT = 330.0f;
+    // the time to wait between angle calculations
+    private const float TIME_BETWEEN_CHECKS = 0.05f;
+    // the direction vector from this object to the mouse during the previous calculation
+    private Vector2 _prevVec;
+    // whether calculations are currently active
+    private bool calculate = false;
 
+    /* Subscribe to the click and release events when object is active*/
     private void OnEnable()
     {
         PlayerController.OnClick += CalculateCircling;
         PlayerController.OnRelease += CheckIfCircled;
     }
-
+    /* Unsubscribe to the click and release events when object is inactive*/
     private void OnDisable()
     {
         PlayerController.OnClick -= CalculateCircling;
@@ -27,13 +47,61 @@ public class CirclingComponent : MonoBehaviour
 		
 	}
 
+    /* Called when player presses mouse, starts angle calculation*/ 
     void CalculateCircling()
     {
-        Debug.Log("Calculating if " + gameObject.name + " is being circled");
+        // indicate that calculation is in progress
+        calculate = true;
+        // reset total angle 
+        _totalAngle = 0;
+        // set value for previous vec to current vector between this object and mouse
+        _prevVec = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+        // begin calculation
+        StartCoroutine(GetAngleSegment());
     }
 
+    /* Called when player releases mouse, checks how far player circled in total*/
     void CheckIfCircled()
     {
-        Debug.Log("Mouse released, check if " + gameObject.name + " should be destroyed");
+        calculate = false;
+    }
+
+    /* Finds the angle of how far the player has circled in TIME_BETWEEN_CHECKS */
+    IEnumerator GetAngleSegment()
+    {
+        // if the player is still dragging...     
+        if (calculate)
+        {
+            // get vector from this object to mouse at this moment
+            Vector2 currentVec = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+            if(currentVec != _prevVec)
+            {
+                // find angle between current vector and the previous check
+                float theta = Mathf.Acos(Vector3.Dot(_prevVec, currentVec) / (Mathf.Abs(_prevVec.magnitude * currentVec.magnitude)));
+                //convert to degrees
+                theta *= (180 / Mathf.PI);
+                // check z component of cross product for sign (gives direction of rotation)
+                int dir = Vector3.Cross(_prevVec, currentVec).z > 0 ? 1 : -1;
+                // add the angle between the vectors to the total amount rotated
+                _totalAngle += (dir * theta);
+                // set previous vector to current vector
+                _prevVec = currentVec;
+            }            
+            // wait before repeating
+            yield return new WaitForSeconds(TIME_BETWEEN_CHECKS);
+            // repeat calculation for next angle segment
+            StartCoroutine(GetAngleSegment());
+        }
+        else
+        {
+            //get absolute value of angle (in case it was a circle in negative direction)
+            _totalAngle = Mathf.Abs(_totalAngle);
+            // if the angle is greater than the angle limit, the object is circled
+            if (_totalAngle >= ANGLE_LIMIT)
+            {
+                Destroy(this.gameObject);
+            }
+        }     
+        yield return null;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * Author: Josh Shucker
+ * Base class for the player
+ * Causes object to follow mouse
+ * Handles trail activation and deactivation on mouse click and release
+ * Calls events on mouse click and release for CirclingComponent functions
+ */
 public class PlayerController : MonoBehaviour {
 
-
+    // Event declaration for click and release events
     public delegate void ClickAction();
     public static event ClickAction OnClick;
     public static event ClickAction OnRelease;
 
-    TrailRenderer trail;
+    // handle to trail component
+    private TrailRenderer _trail;
 
 
 	// Use this for initialization
 	void Start ()
     {
-        trail = GetComponent<TrailRenderer>();
-        trail.enabled = false;
+        /// get trail component and set to inactive at start 
+        _trail = GetComponent<TrailRenderer>();
+        _trail.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -26,26 +34,36 @@ public class PlayerController : MonoBehaviour {
         CheckMouseClick();
 	}
 
+    /* Called in update, causes this object to follow the mouse */
     private void FollowMouse()
-    {
-        Vector3 screenPoint = Input.mousePosition;
+    {   
+        // convert mouse position to world coordinates
         Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // set z component to this objects z to avoid changing its value
         worldPoint.z = transform.position.z;
+        // move this object to the mouse position
         transform.position = worldPoint;
-        //transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
     }
 
+    /* Handles events and trail rendering for mouse click and release*/
     private void CheckMouseClick()
     {
+        // if player clicks left mouse button (or taps)
         if(Input.GetMouseButtonDown(0))
         {
-            trail.enabled = true;
+            // enable the trail renderer
+            _trail.enabled = true;
+            // call the click event
             OnClick();
         }
-        else if(Input.GetMouseButtonUp(0))
+        // if player releases left mouse button (or completes tap)
+        else if (Input.GetMouseButtonUp(0))
         {
-            trail.Clear();
-            trail.enabled = false;
+            // clear the trail from the screen
+            _trail.Clear();
+            // disable the trail renderer
+            _trail.enabled = false;
+            // call the release event
             OnRelease();
         }
     }
