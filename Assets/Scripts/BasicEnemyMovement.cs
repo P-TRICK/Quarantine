70acbf0df2fe5122e7aa6b92a6ce01a6d56fd851
j95alvarez <juanalvarez@Juans-MacBook-Pro.local>
2017-02-14 16:23:09 -0800

    Enemies Move Towards an Object
    Created a basic script that makes enemies move towards an object.

---
+++ Assets/Scripts/BasicEnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyMovement : MonoBehaviour {
+	private GameObject objectToMoveTo;
+
+	[SerializeField]
+	private int speed;
+
+	// Use this for initialization
+	void Start () {
+		// Finds 
+		objectToMoveTo = GameObject.Find("Object");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, objectToMoveTo.transform.position, speed * Time.deltaTime);
+	}
+}
