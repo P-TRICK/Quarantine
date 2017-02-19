02e725988da65823bb15ca62e9e72d5fc1c06d9c
b06to587 <b06to587@gmail.com>
2017-02-19 09:35:23 +0900

    create menu and some buttons

---
+++ Assets/Scripts/TitleMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleMenuScript : MonoBehaviour {
+    public Transform openpanel;
+    public Button exitgame, returnb;
+    // Use this for initialization
+    public void Play(string Scene1) {
+       // Application.LoadLevel(Scene1);
+    }
+
+    public void Options() {
+
+    }
+
+    public void Quit(bool clicked){ 
+     if(clicked == true) {
+         openpanel.gameObject.SetActive(clicked);
+            exitgame.onClick.AddListener(() =>
+            {
+                Application.Quit();
+            });
+            returnb.onClick.AddListener(() =>
+            {
+                openpanel.gameObject.SetActive(false);
+            });
+        } 
+    }
+}
